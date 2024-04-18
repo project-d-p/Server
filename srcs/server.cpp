ba09ad97d70d5f9695a3a50c49b74699ff1c7125
dkglee <ideade9@gmail.com>
2024-04-18 14:21:31 +0900

    FEAT : f0--Server : open listening socket: loopback

---
+++ srcs/server.cpp
@@ -0,0 +1,21 @@
+#include <memory>
+#include "server.hpp"
+
+namespace deulee {
+
+Server::Server(boost::asio::io_context& io_context, unsigned short port)
+	: acceptor_(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
+{
+	Accept();
+}
+
+void Server::Accept() {
+	acceptor_.async_accept([this](boost::system::error_code ec, boost::asio::ip::tcp::socket socket) {
+		if (!ec) {
+			// std::make_shared<Session>(std::move(socket))
+		}
+		Accept();
+	});
+}
+
+} // namespace deulee
