096df0d86e0ac157fe63a8abf2be276edda1d7c8
dkglee <ideade9@gmail.com>
2024-04-26 20:58:35 +0900

    NOTHING

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
-#include <boost/asio.hpp>
+// #include <boost/asio.hpp>
 #include "./srcs/server.hpp"
 #include "./srcs/prototype_factory.hpp"
 
-#define PORT 8080
+#include <iostream>
+#include <vector>
+#include <thread>
+
+#define PORT 4242
 
 int main() {
 	deulee::PrototypeFactory prototype_factory;
-	boost::asio::io_context io_context;
+	// boost::asio::io_context io_context;
 	deulee::Server server(io_context, PORT);
+	std::vector<std::thread> threads;
+	// for (int i = 0; i < 3; i++) {
+	// 	threads.push_back(std::thread([](){
+	// 		io_context.run();
+	// 	}));
+	// }
+	// for (auto& thread : threads) {
+	// 	thread.detach();
+	// }
 	io_context.run();
 	return 0;
 }
