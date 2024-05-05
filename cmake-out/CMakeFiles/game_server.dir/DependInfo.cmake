5a7872689c9307e9046322b76fa00740c85d0eef
dkglee <ideade9@gmail.com>
2024-05-05 14:48:27 +0900

    FIX : Dockerfile CMD : ERROR CLEAR

---
+++ cmake-out/CMakeFiles/game_server.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/deulee/server/main.cpp" "CMakeFiles/game_server.dir/main.cpp.o" "gcc" "CMakeFiles/game_server.dir/main.cpp.o.d"
+  "/home/deulee/server/srcs/call_data.cpp" "CMakeFiles/game_server.dir/srcs/call_data.cpp.o" "gcc" "CMakeFiles/game_server.dir/srcs/call_data.cpp.o.d"
+  "/home/deulee/server/srcs/call_data_attach_player.cpp" "CMakeFiles/game_server.dir/srcs/call_data_attach_player.cpp.o" "gcc" "CMakeFiles/game_server.dir/srcs/call_data_attach_player.cpp.o.d"
+  "/home/deulee/server/srcs/call_data_create_channel.cpp" "CMakeFiles/game_server.dir/srcs/call_data_create_channel.cpp.o" "gcc" "CMakeFiles/game_server.dir/srcs/call_data_create_channel.cpp.o.d"
+  "/home/deulee/server/srcs/channel.cpp" "CMakeFiles/game_server.dir/srcs/channel.cpp.o" "gcc" "CMakeFiles/game_server.dir/srcs/channel.cpp.o.d"
+  "/home/deulee/server/srcs/movement_prototype.cpp" "CMakeFiles/game_server.dir/srcs/movement_prototype.cpp.o" "gcc" "CMakeFiles/game_server.dir/srcs/movement_prototype.cpp.o.d"
+  "/home/deulee/server/srcs/protobuf_mannager.cpp" "CMakeFiles/game_server.dir/srcs/protobuf_mannager.cpp.o" "gcc" "CMakeFiles/game_server.dir/srcs/protobuf_mannager.cpp.o.d"
+  "/home/deulee/server/srcs/prototype.cpp" "CMakeFiles/game_server.dir/srcs/prototype.cpp.o" "gcc" "CMakeFiles/game_server.dir/srcs/prototype.cpp.o.d"
+  "/home/deulee/server/srcs/prototype_factory.cpp" "CMakeFiles/game_server.dir/srcs/prototype_factory.cpp.o" "gcc" "CMakeFiles/game_server.dir/srcs/prototype_factory.cpp.o.d"
+  "/home/deulee/server/srcs/server.cpp" "CMakeFiles/game_server.dir/srcs/server.cpp.o" "gcc" "CMakeFiles/game_server.dir/srcs/server.cpp.o.d"
+  "/home/deulee/server/srcs/session.cpp" "CMakeFiles/game_server.dir/srcs/session.cpp.o" "gcc" "CMakeFiles/game_server.dir/srcs/session.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-out/CMakeFiles/game_server.dir/srcs/prototype.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/game_server.dir/srcs/prototype.cpp.o: \
+ /home/deulee/server/srcs/prototype.cpp /usr/include/stdc-predef.h
